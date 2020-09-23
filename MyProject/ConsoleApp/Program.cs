d5dc007bf782d19e7f5963e1e492e03c77f77509
raskil <raskil@ttu.ee>
2020-09-23 18:46:04 +0300

    Initial consoleApp commit with basic menu

---
+++ MyProject/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MenuSystem;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("=====> RASKIL GAME <=====");
+            
+            var menu = new Menu();
+            menu.RunMenu();
+        }
+    }
+}
